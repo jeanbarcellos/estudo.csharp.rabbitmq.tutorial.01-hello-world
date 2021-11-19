cca2cb7cc45a2d37e4396be99546a51c9fd3a077
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-11-19 08:00:08 -0300

    Receiving

--- Receive/Receive.cs
+++ Receive/Receive.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace Receive
 {
@@ -6,7 +9,38 @@ namespace Receive
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+
+            // Crie e retorne um novo canal, sessão e modelo
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: "hello",
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null
+                );
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+
+                channel.BasicConsume(
+                    queue: "hello",
+                    autoAck: true,
+                    consumer: consumer
+                );
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            };
         }
     }
 }
